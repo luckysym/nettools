e1d6d02aa7b82a2c60b6aef69f048e14495673d8
luckysym <powdog@yeah.net>
2019-03-28 16:42:11 +0800

    sockaddr_from_location, location_free, location_init, socket_open_channel  ok

--- tools/echo-client/echo-client.cpp
+++ tools/echo-client/echo-client.cpp
@@ -2,35 +2,52 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <sys/un.h>
+#include <netinet/tcp.h>
 #include <poll.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#ifndef UNIX_PATH_MAX
+#define UNIX_PATH_MAX 108
+#endif
+
 namespace net {
     
-    struct error_info {
-        const char * str;
-    };
+    const int sockopt_nonblocked  = 1;
+    const int sockopt_linger      = 2;
+    const int sockopt_tcp_nodelay = 4;
+    
+    typedef struct error_info {
+        char * str;
+    } error_t;
 
-    struct socket_attrib {
+    typedef struct socket_attrib {
         int af;
         int type;
         int proto;
-    };
+    } sockattr_t;
 
-    struct location {
-        char * protocol;
+    typedef struct location {
+        char * proto;
         char * host;
         int    port;
         char * path;
-    };
+    } location_t;
 
     /// gets and returns current timestamp in microseconds from epoch-time 
     int64_t now();
 
+    /// init a location struct
+    struct location * location_init(struct location *loc);
+
+    /// free the location string members
+    void location_free(struct location *loc);    
+    
     /// parse url to location, return the localtion pointer if success, null if failed.
     struct location * location_from_url(struct location * loc, const char * url);
 
@@ -46,6 +63,9 @@ namespace net {
     /// \return 解析成功，返回socket_attrib结构指针，否则返回null
     socket_attrib * sockattr_from_protocol(socket_attrib * attr, const char * name);
 
+    /// 根据locaction设置socketaddr
+    socklen_t sockaddr_from_location(sockaddr *paddr, socklen_t len, const location *loc, error_t * err);
+
     /// send message to remote, and return if all data sent or error or timeout, 
     /// returns total bytes sent if succeeded, or return -1 if failed, or 0 if timeout .
     int socket_channel_sendn(int fd, const char *data, int len, int timeout, error_info * err);
@@ -56,7 +76,7 @@ namespace net {
 
     /// create and open a remote socket channel to the remote location, and returns the socket fd.
     /// returns -1 if failed.
-    int socket_open_channel(const location *remote, error_info * err);
+    int socket_open_channel(const location *remote, int options, error_info * err);
 
     /// close socket fd. returns true if success, 
     bool socket_close(int fd, struct error_info *err);
@@ -71,6 +91,74 @@ int64_t net::now()
 }
 
 inline 
+net::location * net::location_init(net::location *loc)
+{
+    loc->proto = nullptr;
+    loc->host = nullptr;
+    loc->port = 0;
+    loc->path = nullptr;
+    return loc;
+}
+
+inline 
+void net::location_free(net::location * loc) {
+    if ( loc->proto ) free((void *)loc->proto);
+    if ( loc->host )  free((void *)loc->host);
+    if ( loc->path )  free((void *)loc->path);
+
+    loc->proto = nullptr;
+    loc->host = nullptr;
+    loc->port = 0;
+    loc->path = nullptr;
+}
+
+inline 
+net::location * net::location_from_url(struct net::location * loc, const char * url)
+{
+        // 解析protocol
+        const char * proto = url;
+        char * p0 = ::strstr((char*)proto, "://");
+        if ( !p0 ) return nullptr;
+        int proto_len = p0 - proto;
+        
+
+        // 解析host
+        const char * host = p0 + 3;
+        int host_len;
+        while ( *host == ' ' ) ++host;
+        if ( *host == '[')  {
+            host += 1;
+            p0 = ::strchr((char*)host, ']');
+            if ( !p0 ) return nullptr;
+        } else {
+            p0 = ::strchr((char*)host, ':');
+            if ( !p0 ) p0 = ::strchr((char*)host, '/');
+        }
+        if ( p0 ) host_len = p0 - host;
+        else host_len = ::strlen(host);
+
+        // 解析port
+        const char * port = (*p0 == ':')?(p0 + 1):nullptr;
+
+        // 解析path
+        const char * path = (*p0 == '/')?p0:nullptr;
+
+        location_free(loc);
+        if ( proto ) {
+            loc->proto = (char *)malloc(proto_len + 1);
+            strncpy(loc->proto, proto, proto_len + 1);
+        }
+        if ( host ) {
+            loc->host = (char*)malloc(host_len + 1);
+            strncpy(loc->host, host, host_len + 1);
+        } 
+        if ( port ) loc->port = atoi(port);
+        if ( path ) loc->path = strdup(path);
+        return loc;
+}
+
+
+inline 
 void net::free_error_info(net::error_info * err)
 {
     if ( err->str ) {
@@ -78,6 +166,89 @@ void net::free_error_info(net::error_info * err)
         err->str = nullptr;
     }
 }
+
+inline 
+int net::socket_open_channel(const net::location * loc, int options, net::error_info * err)
+{
+    // init socket attributes
+    sockattr_t attr;
+    sockattr_t * p = sockattr_from_protocol(&attr, loc->proto);
+    if ( !p ) {
+        if ( err ) {
+            err->str = (char *)malloc(128);
+            snprintf(err->str, 128, "bad socket protocol name: %s", loc->proto);
+        }
+        return -1;
+    }
+
+    // init socket address
+    char addrbuf[128];
+    sockaddr *paddr = (sockaddr*)addrbuf;
+    error_t e2;
+    socklen_t addrlen = sockaddr_from_location((sockaddr*)addrbuf, 128, loc, &e2);
+    if ( addrlen == 0 ) {
+        if ( err ) {
+            err->str = (char *)malloc(128);
+            snprintf(err->str, 128, "bad socket location addr: %s/%s/%d/%s", 
+                loc->proto, loc->host?loc->host:"<none>", loc->port, loc->path?loc->path:"<none>");
+        }
+        free_error_info(&e2);
+        return -1;
+        
+    }
+
+    // create socket
+    int nonblock = (options & sockopt_nonblocked)?SOCK_NONBLOCK:0;
+    int fd = ::socket(attr.af, attr.type | nonblock | SOCK_CLOEXEC, attr.proto);
+    if ( fd == -1 ) {
+        if ( err ) {
+            err->str = (char *)malloc(128);
+            snprintf(err->str, 128, "failed to create socket, %d, %s", errno, strerror(errno));
+        }
+        return -1;
+    }
+
+    // set socket options
+    if ( options & sockopt_tcp_nodelay) {
+        int opt = 1;
+        int r = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt));
+        if ( r == -1 ) {
+            if ( err ) {
+                err->str = (char *)malloc(128);
+                snprintf(err->str, 128, "setsockopt tcp_nodelay error, fd=%d, err=%d, %s", fd, errno, strerror(errno));
+            }
+            close(fd);
+            return -1;
+        }
+    }
+    if ( options & sockopt_linger) {
+        struct linger lg;
+        lg.l_onoff  = 1;
+        lg.l_linger = 30;
+        int r = setsockopt(fd, SOL_SOCKET, SO_LINGER, &lg, sizeof(lg));
+        if ( r == -1 ) {
+            if ( err ) {
+                err->str = (char *)malloc(128);
+                snprintf(err->str, 128, "setsockopt tcp_nodelay error, fd=%d, err=%d, %s", fd, errno, strerror(errno));
+            }
+            close(fd);
+            return -1;
+        }
+    } 
+
+    // connect to remote
+    int r = connect(fd, paddr, addrlen);
+    if ( r == -1 ) {
+        if ( err ) {
+            err->str = (char *)malloc(128);
+            snprintf(err->str, 128, "connect error, fd=%d, err=%d, %s", fd, errno, strerror(errno));
+        }
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+} // end net::socket_open_channel
 
 inline 
 int net::socket_channel_recvn(int fd, char *data, int len, int timeout, net::error_info * err)
@@ -97,21 +268,23 @@ int net::socket_channel_recvn(int fd, char *data, int len, int timeout, net::err
                 if ( n >= 0) {
                     pos += n;
                 } else {
-                    err->str = strdup("failed to receive data");
+                    if ( err ) err->str = strdup("failed to receive data");
                     return -1;
                 }
                 if ( pos == len ) return len;
             } else {
-                err->str = strdup("readable event wait failed");
+                if ( err ) err->str = strdup("readable event wait failed");
                 return -1;
             }
         } else if ( r == 0 ) {
             break;  // timeout
         } else {
             if ( errno != EINTR ) {
-                char errbuf[256];
-                snprintf(errbuf, 256, "wait failed, %s", strerror(errno));
-                err->str = strdup(errbuf);
+                if ( err ) {
+                    char errbuf[256];
+                    snprintf(errbuf, 256, "wait failed, %s", strerror(errno));
+                    err->str = strdup(errbuf);
+                }
                 return -1;  // failed
             }
         }
@@ -155,9 +328,11 @@ int net::socket_channel_sendn(int fd, const char *data, int len, int timeout, ne
             break;  // timeout
         } else {
             if ( errno != EINTR ) {
-                char errbuf[256];
-                snprintf(errbuf, 256, "wait failed, %s", strerror(errno));
-                err->str = strdup(errbuf);
+                if ( err) {
+                    char errbuf[256];
+                    snprintf(errbuf, 256, "wait failed, %s", strerror(errno));
+                    err->str = strdup(errbuf);
+                }
                 return -1;  // failed
             }
         }
@@ -180,6 +355,53 @@ bool net::socket_close( int fd, struct net::error_info * err)
     if ( err ) err->str = ::strdup(strerror(errno));
     return false;
 }
+
+inline
+socklen_t net::sockaddr_from_location(sockaddr *paddr, socklen_t len, const location *loc, error_t * err)
+{
+    // 根据protocol获取af
+    sockattr_t attr;
+    sockattr_t * p = sockattr_from_protocol(&attr, loc->proto);
+    if ( !p ) {
+        if ( err ) {
+            err->str = (char*)malloc(128);
+            snprintf(err->str, 128, "bad location protocol, %s", loc->proto);
+        }
+        return 0;
+    }
+
+    // unix地址
+    if ( attr.af == AF_UNIX ) {
+        sockaddr_un * unaddr = (sockaddr_un*)paddr;
+        if ( loc->path && strlen(loc->path) >= UNIX_PATH_MAX) {
+            if ( err ) {
+                err->str = (char*)malloc(256);
+                snprintf(err->str, 256, "unix path is too long, %s", loc->path);
+            }
+            return 0;
+        }
+        strncpy(unaddr->sun_path, loc->path, UNIX_PATH_MAX);
+        return sizeof(sockaddr_un);
+    }
+
+    // 查找IP主机地址
+    struct addrinfo *res = nullptr;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = attr.af;
+
+    int r = ::getaddrinfo(loc->host, nullptr, &hints, &res);
+    if ( r != 0 ) {
+        if ( err ) {
+            err->str = (char*)malloc(128);
+            snprintf(err->str, 128, "getaddrinfo error, %s, %s", loc->host, gai_strerror(r));
+        }
+        return 0;
+    }
+    memcpy(paddr, res->ai_addr, res->ai_addrlen);     // 地址整体复制输出
+    ((sockaddr_in*)paddr)->sin_port = loc->port;   // 设置端口，in和in6端口字段位置相同
+    return res->ai_addrlen;
+} // end net::sockaddr_from_location
 
 inline 
 net::socket_attrib * net::sockattr_from_protocol(net::socket_attrib * attr, const char * name)
@@ -225,6 +447,7 @@ int main(int argc, char **argv)
 
     // remote url to net::location
     net::location loc;
+    net::location_init(&loc);
     net::location * p = net::location_from_url(&loc, remote);
     if ( !p ) {
         fprintf(stderr, "[error] bad url for pasing, %s\n", remote);
@@ -232,8 +455,9 @@ int main(int argc, char **argv)
     }
 
     // open socket channel
+    int opts = net::sockopt_tcp_nodelay | net::sockopt_linger | net::sockopt_nonblocked;
     struct net::error_info err;
-    int fd = net::socket_open_channel(&loc, &err);
+    int fd = net::socket_open_channel(&loc, opts, &err);
     if ( fd == -1 ) {
         fprintf(stderr, "[error] failed to open socket channel, %s\n", err.str);
         net::free_error_info(&err);
@@ -264,5 +488,7 @@ int main(int argc, char **argv)
     rbuf[bytes - 1] = '\0';
     printf("message received: \n%s\n", rbuf);
     net::socket_close(fd, nullptr);
+    net::location_free(&loc);
+
     return 0;
 }
