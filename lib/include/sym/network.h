8b5941a43f50e5b5ebc58c14d4989c4422626552
luckysym <powdog@yeah.net>
2019-04-05 23:39:41 +0800

    error_info相关操作移出。

--- lib/include/sym/network.h
+++ lib/include/sym/network.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <sym/algorithm.h>
+#include <sym/error.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,7 +15,6 @@
 #include <netdb.h>
 #include <assert.h>
 #include <errno.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -24,67 +24,6 @@
 #endif
 
 #include <pthread.h>
-
-namespace err {
-    typedef struct error_info {
-        char * str;
-        struct error_info * next;
-    } error_t;
-
-    /// init the error info
-    void init_error_info(error_t * err);
-
-    /// free error info members
-    void free_error_info(error_t * err);
-
-    /// push error info to the error object
-    void push_error_info(error_t * err, int size, const char * format, ...);
-
-} // end namespace sl
-
-inline 
-void err::init_error_info(err::error_info * err)
-{
-    err->str = nullptr;
-    err->next = nullptr;
-}
-
-inline 
-void err::free_error_info(err::error_info * err)
-{
-    error_t * p = err->next;
-    while ( p ) {
-        error_t * p1 = p->next;
-        if ( p->str ) {
-            free(p->str);
-            p->str = nullptr;
-        }
-        p->next = nullptr;
-        free(p);
-        p = p1;
-    }
-
-    if ( err->str ) {
-        free((void *)err->str);
-        err->str = nullptr;
-    }
-    err->next = nullptr;
-}
-
-inline 
-void err::push_error_info(err::error_info * err, int size, const char *format, ...) {
-    if( err->str ) {
-        err->next = (error_t*)malloc(sizeof(error_t));
-        err->next->str = err->str;
-    }
-
-    err->str = (char*)malloc(size);
-    va_list ap;
-    va_start(ap, format);
-    vsnprintf(err->str, size, format, ap);
-    va_end(ap);
-}
-
 
 /// namespace for multi-thread 
 namespace mt {
@@ -106,7 +45,6 @@ namespace net {
     const int sockopt_linger      = 2;
     const int sockopt_reuseaddr   = 4;
     const int sockopt_tcp_nodelay = 8;
-
 
     typedef struct socket_attrib {
         int af;
@@ -1142,3 +1080,4 @@ int  net::selector_run(net::selector_t *sel, err::error_t *err)
 
     return r;
 } // net::selector_run
+
