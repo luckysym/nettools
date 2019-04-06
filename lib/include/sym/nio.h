ab5e663ab8525302280d31f9655cd2771456008e
luckysym <powdog@yeah.net>
2019-04-07 00:56:01 +0800

    mutex operations impl.

--- lib/include/sym/nio.h
+++ lib/include/sym/nio.h
@@ -19,6 +19,63 @@ namespace mt {
     bool mutex_unlock(mutex_t * m, err::error_t * err);
 } // end namespace mt
 
+namespace mt {
+
+inline 
+bool mutex_init(mutex_t *m, err::error_t *e)
+{
+    int r = pthread_mutex_init(m, nullptr);
+    if ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_init error, %d, %s", strerror(r));
+        }
+        return false;
+    } 
+}
+
+inline 
+bool mutex_free(mutex_t *m, err::error_t *e) {
+    int r = pthread_mutex_destroy(m);
+    if ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_destroy error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+inline 
+bool mutex_lock(mutex_t *m, err::error_t *e ) {
+    int r = pthread_mutex_lock(m);
+    if  ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_lock error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+inline 
+bool mutex_unlock(mutex_t *m, err::error_t *e ) {
+    int r = pthread_mutex_unlock(m);
+    if  ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_unlock error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+} // end namespace mt
+
 namespace nio
 {
     const int select_none    = 0;
