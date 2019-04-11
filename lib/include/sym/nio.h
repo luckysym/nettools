48c8ceeeddba60fabff6f649609ed54c1b4b0196
luckysym <powdog@yeah.net>
2019-04-11 20:01:23 +0800

    add thread.h, move namespace mt to thread.h

--- lib/include/sym/nio.h
+++ lib/include/sym/nio.h
@@ -3,84 +3,14 @@
 #include <sym/error.h>
 #include <sym/chrono.h>
 #include <sym/algorithm.h>
+#include <sym/thread.h>
 
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
-#include <pthread.h>
 #include <errno.h>
 #include <unistd.h>
 #include <assert.h>
 #include <string.h>
-
-/// namespace for multi-thread 
-namespace mt {
-    typedef pthread_mutex_t mutex_t;
-
-    bool mutex_init(mutex_t *m, err::error_t * err);
-
-    bool mutex_free(mutex_t *m, err::error_t * err);
-
-    bool mutex_lock(mutex_t * m, err::error_t * err);
-
-    bool mutex_unlock(mutex_t * m, err::error_t * err);
-} // end namespace mt
-
-namespace mt {
-
-inline 
-bool mutex_init(mutex_t *m, err::error_t *e)
-{
-    int r = pthread_mutex_init(m, nullptr);
-    if ( r == 0 ) {
-        return true;
-    } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_init error, %d, %s", strerror(r));
-        }
-        return false;
-    } 
-}
-
-inline 
-bool mutex_free(mutex_t *m, err::error_t *e) {
-    int r = pthread_mutex_destroy(m);
-    if ( r == 0 ) {
-        return true;
-    } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_destroy error, %d, %s", strerror(r));
-        }
-        return false;
-    }
-}
-
-inline 
-bool mutex_lock(mutex_t *m, err::error_t *e ) {
-    int r = pthread_mutex_lock(m);
-    if  ( r == 0 ) {
-        return true;
-    } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_lock error, %d, %s", strerror(r));
-        }
-        return false;
-    }
-}
-
-inline 
-bool mutex_unlock(mutex_t *m, err::error_t *e ) {
-    int r = pthread_mutex_unlock(m);
-    if  ( r == 0 ) {
-        return true;
-    } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_unlock error, %d, %s", strerror(r));
-        }
-        return false;
-    }
-}
-
-} // end namespace mt
 
 namespace nio
 {
---
+++ lib/include/sym/thread.h
@@ -0,0 +1,73 @@
+#pragma once 
+
+#include <pthread.h>
+
+/// namespace for multi-thread 
+namespace mt {
+    typedef pthread_mutex_t mutex_t;
+
+    bool mutex_init(mutex_t *m, err::error_t * err);
+
+    bool mutex_free(mutex_t *m, err::error_t * err);
+
+    bool mutex_lock(mutex_t * m, err::error_t * err);
+
+    bool mutex_unlock(mutex_t * m, err::error_t * err);
+} // end namespace mt
+
+namespace mt {
+
+inline 
+bool mutex_init(mutex_t *m, err::error_t *e)
+{
+    int r = pthread_mutex_init(m, nullptr);
+    if ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_init error, %d, %s", strerror(r));
+        }
+        return false;
+    } 
+}
+
+inline 
+bool mutex_free(mutex_t *m, err::error_t *e) {
+    int r = pthread_mutex_destroy(m);
+    if ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_destroy error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+inline 
+bool mutex_lock(mutex_t *m, err::error_t *e ) {
+    int r = pthread_mutex_lock(m);
+    if  ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_lock error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+inline 
+bool mutex_unlock(mutex_t *m, err::error_t *e ) {
+    int r = pthread_mutex_unlock(m);
+    if  ( r == 0 ) {
+        return true;
+    } else {
+        if ( e ) {
+            err::push_error_info(e, 128, "pthread_mutex_unlock error, %d, %s", strerror(r));
+        }
+        return false;
+    }
+}
+
+} // end namespace mt
