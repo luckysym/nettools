9096b0dd4c091ee739ff48da0f9889526fa1d81b
luckysym <powdog@yeah.net>
2019-04-25 11:24:03 +0800

    impl accept new channel

--- tools/srpc_async_server/srpc_async_server.cpp
+++ tools/srpc_async_server/srpc_async_server.cpp
@@ -1,7 +1,12 @@
 #include <sym/srpc.h>
 #include <assert.h>
+#include <map>
 
 #define LOCAL_URL "0.0.0.0:40001"
+
+
+void on_accept(nio::listener_t *lis, int event, const nio::listen_io_param_t *io, void *arg);
+void on_rpc_received(srpc::connection_t * conn, int status, io::buffer_t * buf, void *arg);
 
 int main(int argc, char **argv)
 {
@@ -9,21 +14,68 @@ int main(int argc, char **argv)
 
     const char * local_url = LOCAL_URL;
     if ( argc > 1 ) local_url = argv[1];
+    bool isok ;
 
     err::error_t err;
     err::init_error_info(&err);
 
-    srpc::async_server_t server;
-    bool isok = srpc::async_server_init(&server, &err);
-    assert(isok);
+    std::map<int, nio::channel_t *> channels;  // accepted channel list
 
-    nio::listener_t * lis = srpc::async_server_add_listener(&server, local_url, &err);
-    assert(lis);
+    // 设置本地地址
+    net::location_t clocal;
+    net::location_init(&clocal);
+    net::location_from_url(&clocal, local_url);
 
-    isok = srpc::async_server_run(&server, &err);
-    assert(isok);
+    // 初始化selector
+    nio::selector_t selector;
+    nio::selector_init(&selector, 0, &err);
 
-    isok = srpc::async_server_close(&server, &err);
-    assert(isok);
+    // 初始化并创建listener
+    nio::listener_t listener;
+    nio::listener_init(&listener, &selector, on_accept, &channels);
+    nio::listener_open(&listener, &clocal, &err);
+
+    // 创建并初始化一个channel，并执行异步接收新连接.
+    nio::channel_t *channel = (nio::channel_t *)malloc(sizeof(nio::channel_t));
+    nio::channel_init(channel, &selector, nullptr, nullptr, &err);
+    nio::listener_accept_async(&listener, channel, &err);
+
+    // 执行select run, 等待并处理相关事件。
+    while ( 1 ) {
+        int r = nio::selector_run(&selector, &err);
+    }
+
     return 0;
+}
+
+void on_accept(nio::listener_t *lis, int event, const nio::listen_io_param_t *io, void *arg)
+{
+    std::map<int, nio::channel_t*> *chs = (std::map<int, nio::channel_t*> *)arg;
+
+    err::error_t err;
+    err::init_error_info(&err);
+
+    if ( event == nio::listener_event_accepted ) {
+        SYM_TRACE_VA("on_accepted, accept new channel %d, remote %s:%d", 
+            io->channel->fd, io->remote->host, io->remote->port); 
+
+        (*chs)[io->channel->fd] = io->channel;
+
+        // create new rpc connection, and wait for message arrival
+        srpc::connection_t * conn = srpc::connection_new(io->channel, &err);
+        io::buffer_t rbuf; 
+        io::buffer_alloc(&rbuf, 1024);
+        srpc::receive_async(conn, &rbuf, on_rpc_received, nullptr, &err);
+
+        // accept another channel
+        nio::channel_t * ch = nio::channel_new(io->channel->sel, nullptr, nullptr, &err);
+        nio::listener_accept_async(lis, ch, &err);
+    } else {
+        SYM_TRACE_VA("on_accepted, unknwon event %d", event);
+    }
+}
+
+void on_rpc_received(srpc::connection_t * conn, int status, io::buffer_t * buf, void *arg)
+{
+    SYM_TRACE_VA("on_rpc_received, status %d", status);
 }
