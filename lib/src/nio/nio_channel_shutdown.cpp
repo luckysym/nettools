c0581338ddfdc78a6d6defb7a6466fd2523515ad
luckysym <powdog@yeah.net>
2019-05-20 11:36:59 +0800

    delete unused code files

--- lib/src/nio/nio_channel_shutdown.cpp
+++
@@ -1,9 +0,0 @@
-#include <sym/nio.h>
-
-namespace nio
-{
-    bool channel_shutdown(channel_t *ch, int how, err::error_t *e)
-    {
-        return net::socket_shutdown(ch->fd, how, e);
-    }
-} // end namespace nio
