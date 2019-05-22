c7c09833c3bad47dd4798a7958962997dea2317f
luckysym <powdog@yeah.net>
2019-05-23 01:35:47 +0800

    mutex change error info

--- lib/include/sym/thread.h
+++ lib/include/sym/thread.h
@@ -6,66 +6,58 @@
 namespace mt {
     typedef pthread_mutex_t mutex_t;
 
-    bool mutex_init(mutex_t *m, err::error_t * err);
+    bool mutex_init(mutex_t *m, err::Error * err = nullptr);
 
-    bool mutex_free(mutex_t *m, err::error_t * err);
+    bool mutex_free(mutex_t *m, err::Error * err = nullptr);
 
-    bool mutex_lock(mutex_t * m, err::error_t * err);
+    bool mutex_lock(mutex_t * m, err::Error * err = nullptr);
 
-    bool mutex_unlock(mutex_t * m, err::error_t * err);
+    bool mutex_unlock(mutex_t * m, err::Error * err = nullptr);
 } // end namespace mt
 
 namespace mt {
 
 inline 
-bool mutex_init(mutex_t *m, err::error_t *e)
+bool mutex_init(mutex_t *m, err::Error *e)
 {
     int r = pthread_mutex_init(m, nullptr);
     if ( r == 0 ) {
         return true;
     } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_init error, %d, %s", strerror(r));
-        }
+        if ( e ) *e = err::Error(r, err::dmSystem);
         return false;
     } 
 }
 
 inline 
-bool mutex_free(mutex_t *m, err::error_t *e) {
+bool mutex_free(mutex_t *m, err::Error *e) {
     int r = pthread_mutex_destroy(m);
     if ( r == 0 ) {
         return true;
     } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_destroy error, %d, %s", strerror(r));
-        }
+        if ( e ) *e = err::Error(r, err::dmSystem);
         return false;
     }
 }
 
 inline 
-bool mutex_lock(mutex_t *m, err::error_t *e ) {
+bool mutex_lock(mutex_t *m, err::Error *e ) {
     int r = pthread_mutex_lock(m);
     if  ( r == 0 ) {
         return true;
     } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_lock error, %d, %s", strerror(r));
-        }
+        if ( e ) *e = err::Error(r, err::dmSystem);
         return false;
     }
 }
 
 inline 
-bool mutex_unlock(mutex_t *m, err::error_t *e ) {
+bool mutex_unlock(mutex_t *m, err::Error *e ) {
     int r = pthread_mutex_unlock(m);
     if  ( r == 0 ) {
         return true;
     } else {
-        if ( e ) {
-            err::push_error_info(e, 128, "pthread_mutex_unlock error, %d, %s", strerror(r));
-        }
+        if ( e ) *e = err::Error(r, err::dmSystem);
         return false;
     }
 }
