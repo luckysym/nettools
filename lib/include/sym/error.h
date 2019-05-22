d66879cc3141841e75feff951919c4c46596c846
luckysym <powdog@yeah.net>
2019-05-23 00:14:13 +0800

    delete old code

--- lib/include/sym/error.h
+++ lib/include/sym/error.h
@@ -10,24 +10,6 @@
 
 /// 包含异常错误以及过程跟踪相关数据结构和操作的名称空间。
 namespace err {
-    typedef struct error_info {
-        char * str;
-        struct error_info * next;
-    } error_t;
-
-    /// init the error info
-    void init_error_info(error_t * err);
-
-    /// free error info members
-    void free_error_info(error_t * err);
-
-    /// push error info to the error object
-    void push_error_info(error_t * err, int size, const char * format, ...);
-
-    // move src error to dest
-    void move_error_info(error_t * dest, error_t *src);
-
-    void trace_stderr(const char * file, int line, const char * format, ...);
 
     /// error domain
     enum EnumErrorDomain {
@@ -116,56 +98,4 @@ void err::trace_stderr(const char * file, int line, const char * format, ...)
     va_end(ap);
 
     printf("\n");
-}
-
-inline 
-void err::init_error_info(err::error_info * err)
-{
-    err->str = nullptr;
-    err->next = nullptr;
-}
-
-inline 
-void err::free_error_info(err::error_info * err)
-{
-    error_t * p = err->next;
-    while ( p ) {
-        error_t * p1 = p->next;
-        if ( p->str ) {
-            free(p->str);
-            p->str = nullptr;
-        }
-        p->next = nullptr;
-        free(p);
-        p = p1;
-    }
-
-    if ( err->str ) {
-        free((void *)err->str);
-        err->str = nullptr;
-    }
-    err->next = nullptr;
-}
-
-inline 
-void err::push_error_info(err::error_info * err, int size, const char *format, ...) {
-    if( err->str ) {
-        err->next = (error_t*)malloc(sizeof(error_t));
-        err->next->str = err->str;
-    }
-
-    err->str = (char*)malloc(size);
-    va_list ap;
-    va_start(ap, format);
-    vsnprintf(err->str, size, format, ap);
-    va_end(ap);
-}
-
-inline 
-void err::move_error_info(error_t * dest, error_t *src) {
-    free_error_info(dest);
-    dest->str = src->str;
-    dest->next = src->next;
-    src->str = nullptr;
-    src->next = nullptr;
 }
--- lib/include/sym/io.h
+++ lib/include/sym/io.h
@@ -6,29 +6,6 @@
 
 /// 包含基本IO操作和数据结构的名称空间。
 namespace io {
-    /// IO缓存
-    typedef struct buffer {
-        char *    data;     ///< 内存指针。
-        size_t    size;     ///< 缓存总大小。
-        size_t    begin;    ///< 缓存有效数据开始位置（含）。
-        size_t    end;      ///< 缓存有效数据结束位置（不含）。
-        size_t    limit;    ///< 缓存写入或读取位置限制位置（不含）。
-    } buffer_t;
-
-    /// init the buffer struct
-    buffer_t * buffer_init(buffer_t *buf);
-
-    /// extend the buffer capacity to the new size
-    buffer_t * buffer_alloc(buffer_t *buf, size_t size);
-
-    /// free the buffer members, release its memory
-    void buffer_free(buffer_t *buf);
-
-    /// realloc the buffer with the new capacity size.
-    buffer_t * buffer_realloc(buffer_t *buf, size_t size);
-
-    /// move the data in the buffer to the front
-    buffer_t * buffer_pullup(buffer_t *buf);
 
     /// big endian to host endian
     int32_t btoh(int32_t n);
@@ -220,68 +197,4 @@ int32_t io::htob(int32_t n) {
     SYM_BYTEORDER_CONVERT_32(p0, p1);
     return r;
 # endif
-}
-
-inline 
-io::buffer_t * io::buffer_init(io::buffer_t *buf)
-{
-    buf->data = nullptr;
-    buf->size = buf->begin = buf->end = buf->limit = 0;
-    return buf;
-}
-
-inline 
-void io::buffer_free(io::buffer_t *buf) 
-{
-    if ( buf->data ) free(buf->data);
-    buf->data  = nullptr;
-    buf->size  = 0;
-    buf->limit = 0;
-    buf->begin = 0;
-    buf->end   = 0;
-}
-
-inline 
-io::buffer_t * io::buffer_alloc(io::buffer_t *buf, size_t size)
-{
-    if ( size > 0 ) {
-        buf->data = (char *)malloc(size);
-        assert(buf->data);
-    } else {
-        buf->data = nullptr;
-    }
-    buf->size  = buf->limit = size;
-    buf->begin = 0;
-    buf->end   = 0;
-    return buf;
-}
-
-inline 
-io::buffer_t * io::buffer_realloc(io::buffer_t *buf, size_t size)
-{
-    if ( size == 0 ) {
-        io::buffer_free(buf);
-        return buf;
-    }
-    void * p = realloc(buf->data, size);
-    buf->data = (char*)p;
-    buf->size = size;
-    if ( buf->begin > buf->size ) buf->begin = buf->size;
-    if ( buf->end > buf->size ) buf->end = buf->size;
-    if ( buf->limit > buf->end ) buf->limit = buf->end;
-
-    return buf;
-}
-
-inline 
-io::buffer_t * io::buffer_pullup(io::buffer_t *buf) 
-{
-    if ( buf->begin == 0 ) return buf;
-
-    size_t len = buf->end - buf->begin;
-    void * p = memmove(buf->data, buf->data + buf->begin, len);
-    assert(p == buf->data);
-    buf->begin = 0;
-    buf->end = len;
-    return buf;
 }
