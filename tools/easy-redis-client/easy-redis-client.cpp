deac422bb3e4460ebe030776d3b10862c01c7fe1
luckysym <powdog@yeah.net>
2019-06-04 20:03:09 +0800

    simple redis client test

--- tools/easy-redis-client/easy-redis-client.cpp
+++ tools/easy-redis-client/easy-redis-client.cpp
@@ -15,11 +15,56 @@ int on_send(const char * cmd, int len, int timeout, err::Error *e)
 
 int on_recv(char * cmd, int len, int timeout, err::Error *e)
 {
+    static int i = 0;
     SYM_TRACE_VA("on recv: %d", len);
-    strcpy(cmd, "+OK\r\n");
-    return strlen(cmd);    
+    // strcpy(cmd, "+OK\r\n");
+    // strcpy(cmd, "-Error message\r\n");
+    /*
+    if ( i == 0 ) {
+        strcpy(cmd, "-Error ");
+        i = 1;
+    } else {
+        strcpy(cmd, "Message\r\n");
+    }
+    */
+    
+    // strcpy(cmd, ":1234567890\r\n");
+    // strcpy(cmd, "$10\r\n123456\r\n78\r\n");
+    // strcpy(cmd, "*-1\r\n");
+    // strcpy(cmd, "*1\r\n+OK\r\n");
+    // strcpy(cmd, "*2\r\n+OK\r\n:987654321\r\n");
+    strcpy(cmd, "*5\r\n"
+                  "+OK\r\n"
+                  ":987654321\r\n"
+                  "$5\r\nhello\r\n"
+                  "*2\r\n"
+                    "$6\r\nworld!\r\n"
+                    ":55667788\r\n"
+                  "$-1\r\n");
+
+    return strlen(cmd);
 }
 
+void print_redis_value(redis::Value * value)
+{
+    if ( value->type() == redis::Value::vtString ) {
+        printf("%s\n", value->getString().c_str());
+    } 
+    else if ( value->type() == redis::Value::vtInteger ) {
+        printf("%lld\n", value->getInt());
+    } 
+    else if ( value->type() == redis::Value::vtList ) {
+        auto it = value->list().begin();
+        printf("list count: %d\n", (int)value->list().size());
+        for( ; it != value->list().end(); ++it) print_redis_value(&(*it));
+    }
+    else if ( value->type() == redis::Value::vtStatus ) {
+        printf("%s\n", value->getString().c_str());
+    }
+    else if ( value->type() == redis::Value::vtNull ) {
+        printf("(NULL)\n");
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -44,8 +89,9 @@ int main(int argc, char **argv)
     command.setText("set a 100");
     command.execute(&result, 1000, &e);
     
-    SYM_TRACE_VA("command result: %s", result.getString().c_str() );
-    sleep(30);
+    printf("type of result: %d\n", result.type());
+    print_redis_value(&result);
+    sleep(1);
 
     channel.close();
 
